3bd3a9a9dd3c16b958791dca0d8b61664e8ef0d4
annacanal <31701548+annacanal@users.noreply.github.com>
2017-12-28 14:39:17 +0100

    Update kernel_ssk.py

--- kernel_ssk.py
+++ kernel_ssk.py
@@ -1,12 +1,12 @@
 def compute(idx_a,idx_b):
 
-	// sanity check
-	// allocating memory for computing K' (Kp)
-	// initialize for 0 subsequence length for both the strings
-	// computing of the K' (Kp) function using equations
-	// shown in Lodhi et. al. See the class documentation for
-	// definitions of Kp and Kpp
-	// compute the kernel function
+	# sanity check
+	# allocating memory for computing K' (Kp)
+	# initialize for 0 subsequence length for both the strings
+	# computing of the K' (Kp) function using equations
+	# shown in Lodhi et. al. See the class documentation for
+	# definitions of Kp and Kpp
+	# compute the kernel function
 
-	// cleanup
+	/# cleanup
 	return K;
