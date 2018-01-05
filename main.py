05a4320b0228627ce45ba690c51fe6fca4c380c1
annacanal <31701548+annacanal@users.noreply.github.com>
2018-01-05 14:55:37 +0100

    Main

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ from sklearn.svm import LinearSVC
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.metrics import f1_score, precision_score, recall_score,precision_recall_fscore_support
 import numpy as np
+import kernel_wk
+import kernel_ngk
+import kernel_ssk
+import _pickle as pickle
+
 
 cachedStopWords = stopwords.words("english")
-
 ACQ = 'acq'
 CORN = 'corn'
 CRUDE = 'crude'
@@ -22,39 +26,36 @@ CATEGORIES = [ACQ, CORN, CRUDE, EARN]
 SPLITS = [TRAIN, TEST]
 
 SIZES = {
-  TRAIN: {
-    ACQ: 114,
-    CORN: 38,
-    CRUDE: 76,
-    EARN: 152
-  },
-  TEST: {
-    ACQ: 25,
-    CORN: 10,
-    CRUDE: 15,
-    EARN: 40
-  }
+    TRAIN: {
+        ACQ: 114,
+        CORN: 38,
+        CRUDE: 76,
+        EARN: 152
+    },
+    TEST: {
+        ACQ: 25,
+        CORN: 10,
+        CRUDE: 15,
+        EARN: 40
+    }
 }
 
 
 ########################### PREPROCESSING METHODS
-def initialize(shuffle = True):
-    dataset = load_dataset(shuffle)
-    # train_docs_id = list(filter(lambda doc: doc.startswith("train"), documents))
-    # test_docs_id = list(filter(lambda doc: doc.startswith("test"), documents))
-    #
-    # train_docs = [reuters.raw(doc_id) for doc_id in train_docs_id]
-    # test_docs = [reuters.raw(doc_id) for doc_id in test_docs_id]
+def load_dataset(filename):
+    with open(filename, 'rb') as f:
+        dataset = pickle.load(f)
+    return dataset
+
+def initialize():
+    filename = 'dataset/subset_470.pkl'
+    dataset = load_dataset(filename)
     labels = {
         'acq': 0,
         'corn': 1,
         'crude': 2,
         'earn': 3
     }
-    # train_docs_id = map(lambda tuple: tuple[1], dataset[TRAIN])
-    # test_docs_id = map(lambda tuple: tuple[1], dataset[TEST])
-    # train_docs = map(lambda tuple: tuple[0], dataset[TRAIN])
-    # test_docs =  map(lambda tuple: tuple[0], dataset[TEST])
 
     #select documents and labels
     train_docs = [tuple[0] for tuple in dataset[TRAIN]]
@@ -63,44 +64,17 @@ def initialize(shuffle = True):
     test_labels = np.array([labels[tuple[1]] for tuple in dataset[TEST]])
     return train_docs, train_labels, test_docs, test_labels
 
-def load_dataset(shuffle=True):
-  if shuffle:
-    print('Shuffling dataset')
-  dataset = {}
-  dataset_ids = {}
-  for split in SPLITS:
-    dataset[split] = []
-    dataset_ids[split] = []
-    for category in CATEGORIES:
-      print('Loading "{}" split "{}" category ({} examples)'.format(split, category, SIZES[split][category]))
-      # File names are preceded by 'train' or 'test'
-      raw_docs = [reuters.raw(doc_id) for doc_id in reuters.fileids(category) if split in doc_id]
-      raw_ids = [reuters.fileids() for doc_id in reuters.fileids(category) if split in doc_id]
-      if shuffle:
-        random.shuffle(raw_docs)
-      counter = 0
-      for raw_doc in raw_docs:
-        if len(raw_doc) > 15:
-          dataset[split].append((raw_doc, category))
-          counter += 1
-      for raw_id in raw_ids:
-        if len(raw_id) > 15:
-          dataset_ids[split].append((raw_id, category))
-        if counter >= SIZES[split][category]:
-          break
-  return dataset
-
 
 ########################### USING THE KERNELS
-def extract_features(kernel, train_docs, train_labels, test_docs, test_labels):
+def extract_features(kernel_type, train_docs, train_labels, test_docs, test_labels):
 
     # Tokenisation
-    if kernel == 'wk':
-        vectorizer = TfidfVectorizer(tokenizer=word_kernel)
-    elif kernel == 'ngram':
-        vectorizer = TfidfVectorizer(tokenizer=ngram_kernel)
-    elif kernel == 'ssk':
-        vectorizer = TfidfVectorizer(tokenizer=ssk_kernel)
+    if kernel_type == 'wk':
+        vectorizer = TfidfVectorizer(tokenizer=kernel_wk.word_kernel)
+    elif kernel_type == 'ngram':
+        vectorizer = TfidfVectorizer(tokenizer=kernel_ngk) #not implemented yet
+    elif kernel_type == 'ssk':
+        vectorizer = TfidfVectorizer(tokenizer=kernel_ssk) #not implemented yet
     else:
         print('Kernel not found.')
 
@@ -108,43 +82,22 @@ def extract_features(kernel, train_docs, train_labels, test_docs, test_labels):
     vectorised_train_documents = vectorizer.fit_transform(train_docs)
     vectorised_test_documents = vectorizer.transform(test_docs)
 
-    # Transform multilabel labels
-    # mlb = MultiLabelBinarizer()
-    # train_labels = mlb.fit_transform([reuters.categories(doc_id) for doc_id in train_docs_id])
-    # test_labels = mlb.transform([reuters.categories(doc_id) for doc_id in test_docs_id])
     return (vectorised_train_documents, train_labels, vectorised_test_documents, test_labels)
 
-# WORD KERNEL
-def word_kernel(text):
-    min_length = 3
-    words = map(lambda word: word.lower(), word_tokenize(text))
-    words = [word for word in words if word not in cachedStopWords]
-    tokens = (list(map(lambda token: PorterStemmer().stem(token), words)))
-    p = re.compile('[a-zA-Z]+')
-    filtered_tokens = list(filter(lambda token: p.match(token) and len(token) >= min_length, tokens))
-    return filtered_tokens
-
-# N-GRAM KERNEL
-def ngram_kernel():
-    # TO DO
-    return None
-
-# SSK KERNEL
-def ssk_kernel():
-    # TO DO
-    return None
 
 
 
 ############################ CLASSIFIER
 def train_classifier(train_docs, train_labels):
+   # kernel_wk.compute(train_docs, test_docs)
     classifier = OneVsRestClassifier(LinearSVC(random_state=42))
-    classifier.fit(train_docs, train_labels)
+    classifier.fit(train_docs, train_labels) #instead of train_docs -> kernel_train
+    #classifier.fit(kernel_train, train_labels) #instead of train_docs -> kernel_train
     return classifier
 
 
 ############################ EVALUATION FUNCTIONS
-def evaluate(test_labels, predictions):
+def evaluate_macro_micro(test_labels, predictions):
     precision = precision_score(test_labels, predictions, average='micro')
     recall = recall_score(test_labels, predictions, average='micro')
     f1 = f1_score(test_labels, predictions, average='micro')
@@ -202,21 +155,22 @@ def evaluate(test_labels, predictions):
 # Prepare Dataset
 train_docs_raw, train_labels_raw, test_docs_raw, test_labels_raw = initialize()
 # Get the feature according to the chosen kernel
-kernel = 'wk'
-train_docs, train_labels, test_docs, test_labels = extract_features(kernel,
+kernel_type = 'wk'
+train_docs, train_labels, test_docs, test_labels = extract_features(kernel_type,
                                             train_docs_raw, train_labels_raw, test_docs_raw, test_labels_raw)
+
+#kernel_train = kernel_wk.compute(train_docs,train_docs)
 # Train the model
-model = train_classifier(train_docs, train_labels)
+model = train_classifier(train_docs, train_labels) #instead of train_docs -> kernel_train
 
+#kernel_test = kernel_wk.compute(train_docs,test_docs)
 # Predict
-predictions = model.predict(test_docs)
-
+predictions = model.predict(test_docs)  #instead of test_docs -> kernel_test
+#predictions = model.predict(kernel_test)
 # Evaluate predictions
 evaluate(test_labels, predictions)
-
-
-#evaluate_acq(test_labels, predictions)
-#evaluate_earn(test_labels, predictions)
-#evaluate_corn(test_labels, predictions)
-#evaluate_crude(test_labels, predictions)
-#evaluate(test_labels, predictions)
+#
+# evaluate_acq(test_labels, predictions)
+# evaluate_earn(test_labels, predictions)
+# evaluate_corn(test_labels, predictions)
+# evaluate_crude(test_labels, predictions)
