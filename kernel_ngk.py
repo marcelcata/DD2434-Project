ed8f682311a6fa1042d1f688ac330ba9100cd528
Maxime Hanot <maxime.hanot@student.uclouvain.be>
2018-01-09 10:34:09 +0100

    kernel NGK

--- kernel_ngk.py
+++ kernel_ngk.py
@@ -1,7 +1,14 @@
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # N-GRAM KERNEL
 
-#Compute kernel for strings s and t
-def compute(s,t):
-#to do
-    return None
+#Compute kernel for documents s and t
+def compute(s,t,n):
+    tfidfVectorizer = TfidfVectorizer(analyzer = "char",
+                                        tokenizer = None,
+                                        preprocessor = None,
+                                        ngram_range=(n, n))
+    docs = tfidfVectorizer.fit_transform([s,t])
+    docs = docs.toarray()
+    return np.dot(docs[0], docs[1])
