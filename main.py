75e73eb42952671a3095eea88da38da2f566c1e1
annacanal <31701548+annacanal@users.noreply.github.com>
2018-01-13 18:46:31 +0100

    Re-structured

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from nltk.corpus import stopwords, reuters
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
 import re
-import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.svm import LinearSVC, SVC
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.metrics import f1_score, precision_score, recall_score,precision_recall_fscore_support
 import numpy as np
 import kernel_wk
 import kernel_ngk
 import kernel_ssk
+import evaluate_functions
+import combined_kernels
 import _pickle as pickle
 import string
 
@@ -29,21 +29,6 @@ SPLITS = [TRAIN, TEST]
 LOAD_MATRICES = 0
 SAVE_MATRICES = 1
 
-SIZES = {
-    TRAIN: {
-        ACQ: 114,
-        CORN: 38,
-        CRUDE: 76,
-        EARN: 152
-    },
-    TEST: {
-        ACQ: 25,
-        CORN: 10,
-        CRUDE: 15,
-        EARN: 40
-    }
-}
-
 
 ########################### PREPROCESSING METHODS
 def load_dataset(filename):
@@ -53,6 +38,7 @@ def load_dataset(filename):
 
 def initialize():
     filename = 'dataset/subset_470.pkl'
+    filename2 = 'dataset/subset_ssk.pkl'
     dataset = load_dataset(filename)
     labels = {
         'acq': 0,
@@ -109,115 +95,35 @@ def gram_matrices(kernel_type, train_docs, train_labels, test_docs, test_labels,
     return train_matrix, train_labels, test_matrix, test_labels
 
 
-########################### USING THE KERNELS
-def extract_features(kernel_type, train_docs, train_labels, test_docs, test_labels):
-
-    # Tokenisation
-    if kernel_type == 'wk':
-        vectorizer = TfidfVectorizer(tokenizer=kernel_wk.word_kernel)
-    elif kernel_type == 'ngram':
-        vectorizer = TfidfVectorizer(tokenizer=kernel_ngk) #not implemented yet
-    elif kernel_type == 'ssk':
-        vectorizer = TfidfVectorizer(tokenizer=kernel_ssk) #not implemented yet
-    else:
-        print('Kernel not found.')
-
-    # Learn and transform train documents
-    vectorised_train_documents = vectorizer.fit_transform(train_docs)
-    vectorised_test_documents = vectorizer.transform(test_docs)
-
-    return (vectorised_train_documents, train_labels, vectorised_test_documents, test_labels)
-
-
-
-
 ############################ CLASSIFIER
-def train_classifier(train_docs, train_labels):
-   # kernel_wk.compute(train_docs, test_docs)
-    #classifier = OneVsRestClassifier(LinearSVC(random_state=42))
+def train_classifier(train_matrix, train_labels):
     classifier = SVC(kernel='precomputed')
-    classifier.fit(train_docs, train_labels) #instead of train_docs -> kernel_train
-    #classifier.fit(kernel_train, train_labels) #instead of train_docs -> kernel_train
+    classifier.fit(train_matrix, train_labels)
     return classifier
 
-
-############################ EVALUATION FUNCTIONS
-def evaluate_macro_micro(test_labels, predictions):
-    precision = precision_score(test_labels, predictions, average='micro')
-    recall = recall_score(test_labels, predictions, average='micro')
-    f1 = f1_score(test_labels, predictions, average='micro')
-    print("Micro-average quality numbers")
-    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
-
-    precision = precision_score(test_labels, predictions, average='macro')
-    recall = recall_score(test_labels, predictions, average='macro')
-    f1 = f1_score(test_labels, predictions, average='macro')
-
-    print("Macro-average quality numbers")
-    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
-
-def evaluate_acq(test_labels, predictions):
-    labels = [0]
-    precision = precision_score(test_labels, predictions,labels,average='macro')
-    recall = recall_score(test_labels, predictions,labels, average='macro')
-    f1 = f1_score(test_labels, predictions,labels,average='macro')
-    print("acq")
-    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
-
-def evaluate_earn(test_labels, predictions):
-    labels = [3]
-    precision = precision_score(test_labels, predictions,labels,average='macro')
-    recall = recall_score(test_labels, predictions,labels, average='macro')
-    f1 = f1_score(test_labels, predictions,labels,average='macro')
-    print("earn")
-    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
-
-def evaluate_corn(test_labels, predictions):
-    labels = [1]
-    precision = precision_score(test_labels, predictions,labels,average='macro')
-    recall = recall_score(test_labels, predictions,labels, average='macro')
-    f1 = f1_score(test_labels, predictions,labels,average='macro')
-    print("corn")
-    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
-
-def evaluate_crude(test_labels, predictions):
-    labels = [2]
-    precision = precision_score(test_labels, predictions,labels,average='macro')
-    recall = recall_score(test_labels, predictions,labels, average='macro')
-    f1 = f1_score(test_labels, predictions,labels,average='macro')
-    print("crude")
-    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
-
-def evaluate(test_labels, predictions):
-    precision, recall, fscore, support = precision_recall_fscore_support(test_labels, predictions)
-    print("eval")
-    print('precision: {}'.format(precision))
-    print('recall: {}'.format(recall))
-    print('fscore: {}'.format(fscore))
-
-
+###################### MAIN #####################
 # Prepare Dataset
 train_docs_raw, train_labels_raw, test_docs_raw, test_labels_raw = initialize()
 # Get the feature according to the chosen kernel
-kernel_type = 'ssk'
-k = 2
+kernel_type = 'wk'
+k = 1
 m_lambda = 0.5
-#train_docs, train_labels, test_docs, test_labels = extract_features(kernel_type,
-#                                            train_docs_raw, train_labels_raw, test_docs_raw, test_labels_raw)
+
+
 
 train_docs_raw = [preprocessing(doc) for doc in train_docs_raw]
 test_docs_raw = [preprocessing(doc) for doc in test_docs_raw]
 
 if LOAD_MATRICES:
     if kernel_type == 'wk':
-        train_matrix = np.load('Gmatrix_train_'+kernel_type+'.npy')
-        test_docs = np.load('Gmatrix_test_'+kernel_type+'.npy')
+        train_matrix = np.load('gram_matrices/Gmatrix_train_'+kernel_type+'.npy')
+        test_docs = np.load('gram_matrices/Gmatrix_test_'+kernel_type+'.npy')
     if kernel_type == 'ngk':
-        train_matrix = np.load('Gmatrix_train_' + kernel_type + '.npy')
-        test_docs = np.load('Gmatrix_test_' + kernel_type + '.npy')
+        train_matrix = np.load('gram_matrices/Gmatrix_train_' + kernel_type + '.npy')
+        test_docs = np.load('gram_matrices/Gmatrix_test_' + kernel_type + '.npy')
     if kernel_type == 'ssk':
-        train_matrix = np.load('Gmatrix_train_'+kernel_type+str(k)+str(m_lambda)+'.npy')
-        test_docs = np.load('Gmatrix_test_'+kernel_type+str(k)+str(m_lambda)+'.npy')
+        train_matrix = np.load('gram_matrices/Gmatrix_train_'+kernel_type+str(k)+str(m_lambda)+'.npy')
+        test_docs = np.load('gram_matrices/Gmatrix_test_'+kernel_type+str(k)+str(m_lambda)+'.npy')
     train_labels = train_labels_raw
     test_labels = test_labels_raw
 else:
@@ -225,22 +131,22 @@ else:
                                             train_docs_raw, train_labels_raw, test_docs_raw, test_labels_raw, k, m_lambda)
 if SAVE_MATRICES:
     if kernel_type == 'wk':
-        np.save('Gmatrix_train_'+kernel_type,train_matrix)
-        np.save('Gmatrix_test_'+kernel_type,test_matrix)
+        np.save('gram_matrices/Gmatrix_train_'+kernel_type,train_matrix)
+        np.save('gram_matrices/Gmatrix_test_'+kernel_type,test_matrix)
     if kernel_type == 'ngk':
-        np.save('Gmatrix_train_'+kernel_type+str(k),train_matrix)
-        np.save('Gmatrix_test_'+kernel_type+str(k),test_matrix)
+        np.save('gram_matrices/Gmatrix_train_'+kernel_type+str(k),train_matrix)
+        np.save('gram_matrices/Gmatrix_test_'+kernel_type+str(k),test_matrix)
     if kernel_type == 'ssk':
-        np.save('Gmatrix_train_' + kernel_type + str(k)+str(m_lambda), train_matrix)
-        np.save('Gmatrix_test_' + kernel_type + str(k)+str(m_lambda), test_matrix)
+        np.save('gram_matrices/Gmatrix_train_' + kernel_type + str(k)+str(m_lambda), train_matrix)
+        np.save('gram_matrices/Gmatrix_test_' + kernel_type + str(k)+str(m_lambda), test_matrix)
 
 # Train the model
 model = train_classifier(train_matrix, train_labels)
 # Predict
 predictions = model.predict(test_matrix)
-#predictions = model.predict(kernel_test)
+
 # Evaluate predictions
-evaluate(test_labels, predictions)
+evaluate_functions.evaluate(test_labels, predictions)
 #
 # evaluate_acq(test_labels, predictions)
 # evaluate_earn(test_labels, predictions)
