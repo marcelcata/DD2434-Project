3ac768f587c62b676931c3c0e258add08a4ce845
annacanal <31701548+annacanal@users.noreply.github.com>
2018-01-13 16:13:18 +0100

    Add normalisation

--- kernel_ssk.py
+++ kernel_ssk.py
@@ -1,45 +1,42 @@
+# SSK KERNEL
 #Efficient Computation of SSK
-
 import numpy as np
-
-#include <shogun/kernel/string/SubsequenceStringKernel.h>
-#include <shogun/kernel/normalizer/SqrtDiagKernelNormalizer.h>
-#include <shogun/features/StringFeatures.h>
-
-#using namespace shogun;
+import math
 
 #Compute kernel for strings s and t
 def compute(s,t,k,m_lambda):
-	len_s = len(s)
-	len_t = len(t)
-	max_len= k #max lenght
-	#m_lambda = 0.5 #weights
-	Kp=np.zeros([max_len+1,len_s,len_t]) # kp = k'
-	Kpp=[] # Kpp = k''
+    Kst= computeK(s,t,k,m_lambda)
+    Kss= computeK(s,s,k,m_lambda)
+    Ktt= computeK(t,t,k,m_lambda)
+    denominator = math.sqrt(Kss*Ktt) + 10e-20
+    return Kst / denominator
 
 
-	# allocating memory for computing K' (Kp)  ->> ??
-	# initialize for 0 subsequence length for both the strings
-	for i in range(len_s):
-		for j in range(len_t):
-			Kp[0][i][j]= 1.0
+def computeK(s,t,k,m_lambda):
+    len_s = len(s)
+    len_t = len(t)
+    max_len = k  # max lenght
+    Kp = np.zeros([max_len + 1, len_s, len_t])  # kp = k'
+    Kpp = 0.0  # Kpp = k''
 
-	# computing of the K' (Kp) function using equations
-	# shown in Lodhi et. al. See the class documentation for
-	# definitions of Kp and Kpp
-	for i in range(max_len):
-		for j in range(len_s - 1):
-			Kpp=0.0
-			for x in range(len_t - 1):
-				Kpp=m_lambda*(Kpp+m_lambda*(s[j]==t[x])*Kp[i][j][x])
-				Kp[i+1][j+1][x+1]=m_lambda*Kp[i+1][j][x+1]+Kpp
+    # initialize for 0 subsequence length for both the strings
+    for i in range(len_s):
+        for j in range(len_t):
+            Kp[0][i][j] = 1.0
 
-	# compute the kernel function
-	K=0.0;
-	for i in range(max_len):
-		for j in range(len_s):
-			for x in range(len_t):
-				K+=m_lambda*m_lambda*(s[j]==t[x])*Kp[i][j][x]
+    # computing of the K' (Kp) function using equations shown in Lodhi et. al.
+    for i in range(max_len):
+        for j in range(len_s - 1):
+            Kpp = 0.0
+            for x in range(len_t - 1):
+                Kpp = m_lambda * (Kpp + m_lambda * (s[j] == t[x]) * Kp[i][j][x])
+                Kp[i + 1][j + 1][x + 1] = m_lambda * Kp[i + 1][j][x + 1] + Kpp
 
-	# cleanup  ->> ??
-	return K;
+    # compute the kernel function
+    K = 0.0;
+    for i in range(max_len):
+        for j in range(len_s):
+            for x in range(len_t):
+                K += m_lambda * m_lambda * (s[j] == t[x]) * Kp[i][j][x]
+
+    return K;
