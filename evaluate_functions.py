ae5145eb92980cd41ce894fa8809b1f46342c8a2
annacanal <31701548+annacanal@users.noreply.github.com>
2018-01-14 22:17:05 +0100

    Add files via upload

--- evaluate_functions.py
+++ evaluate_functions.py
@@ -22,6 +22,7 @@ def evaluate_acq(test_labels, predictions):
     f1 = f1_score(test_labels, predictions,labels,average='macro')
     print("acq")
     print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
+    return precision, recall, f1
 
 def evaluate_earn(test_labels, predictions):
     labels = [3]
@@ -30,6 +31,7 @@ def evaluate_earn(test_labels, predictions):
     f1 = f1_score(test_labels, predictions,labels,average='macro')
     print("earn")
     print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
+    return precision, recall, f1
 
 def evaluate_corn(test_labels, predictions):
     labels = [1]
@@ -38,6 +40,7 @@ def evaluate_corn(test_labels, predictions):
     f1 = f1_score(test_labels, predictions,labels,average='macro')
     print("corn")
     print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
+    return precision, recall, f1
 
 def evaluate_crude(test_labels, predictions):
     labels = [2]
@@ -46,6 +49,7 @@ def evaluate_crude(test_labels, predictions):
     f1 = f1_score(test_labels, predictions,labels,average='macro')
     print("crude")
     print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
+    return precision, recall, f1
 
 def evaluate(test_labels, predictions):
     precision, recall, f1score, support = precision_recall_fscore_support(test_labels, predictions)
